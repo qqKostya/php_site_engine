669e98361e3628d0df7649efec72047ebf20d3c9
qqKazakov <leap74rus@gmail.com>
2023-08-23 07:47:33 +0300

    add: templates file and dir

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
-echo $url;
+
+$layout  = file_get_contents('layout.php');
+$content = file_get_contents('./view/' . 'page1.php');
+
+$layout = str_replace('{{ content }}', $content, $layout);
+
+echo $layout;
---
+++ layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>title</title>
+</head>
+
+<body>
+    <header>
+        header
+    </header>
+    <main>
+        {{ content }}
+    </main>
+    <header>
+        footer
+    </header>
+</body>
+
+</html>
