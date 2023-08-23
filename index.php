046f12f76906504a7ff0b7b45530ad67bf723045
qqKazakov <leap74rus@gmail.com>
2023-08-23 08:38:59 +0300

    add: 404 page and script

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ preg_match('#{{ title: "(.+?)" }}#', $content, $match);
 $title = $match[1];
 $content = preg_replace('#{{ title: "(.+?)" }}#', '', $content);
 
-$path = 'view' . $url . '.php';
 
 $layout = str_replace('{{ title }}', $title, $layout);
 $layout = str_replace('{{ content }}', $content, $layout);
 echo $layout;
+
+$path = 'view' . $url . '.php';
+
+if (file_exists($path)) {
+    $content = file_get_contents($path);
+} else {
+    header('HTTP/1.0 404 Not Found');
+    $content = file_get_contents('view/404.php');
+}
