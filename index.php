2701bddd316c18f88e00cc8273c2bdd2b051b763
qqKazakov <leap74rus@gmail.com>
2023-08-23 08:07:13 +0300

    add: render title

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ $url = $_SERVER['REQUEST_URI'];
 $layout  = file_get_contents('layout.php');
 $content = file_get_contents('./view/' . 'page1.php');
 
-$layout = str_replace('{{ content }}', $content, $layout);
+preg_match('#{{ title: "(.+?)" }}#', $content, $match);
+$title = $match[1];
 
+$layout = str_replace('{{ title }}', $title, $layout);
+
+$content = preg_replace('#{{ title: "(.+?)" }}#', '', $content);
+
+$layout = str_replace('{{ content }}', $content, $layout);
 echo $layout;
---
+++ titles.php
@@ -0,0 +1,6 @@
+<?php
+	return [
+		'/page1'         => 'page 1 title',
+		'/dir/page2'     => 'page 2 title',
+		'/dir/sub/page3' => 'page 3 title',
+	];
