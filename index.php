8d7aa3d1bab71fab66785bc8d13524be11ba697d
qqKazakov <leap74rus@gmail.com>
2023-08-23 08:19:59 +0300

    fix: path to file and url

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 $url = $_SERVER['REQUEST_URI'];
 
 $layout  = file_get_contents('layout.php');
-$content = file_get_contents('./view/' . 'page1.php');
+$content = file_get_contents('view' . $url . '.php');
 
 preg_match('#{{ title: "(.+?)" }}#', $content, $match);
 $title = $match[1];
-
-$layout = str_replace('{{ title }}', $title, $layout);
-
 $content = preg_replace('#{{ title: "(.+?)" }}#', '', $content);
 
+$path = 'view' . $url . '.php';
+
+$layout = str_replace('{{ title }}', $title, $layout);
 $layout = str_replace('{{ content }}', $content, $layout);
 echo $layout;
